364dba67409eace1663b51899638c57161e6156b
Krzysztof Oleszczuk <s33626@pjwstk.edu.pl>
2026-03-18 15:14:45 +0100

    Add new method to calculate median in an array

--- zad1/src/StatisticHelper.java
+++ zad1/src/StatisticHelper.java
@@ -12,4 +12,17 @@ public class StatisticHelper {
             return 0;
         }
     }
+    public static double calcMedian(int[] numbers){
+        if(numbers.length != 0){
+            java.util.Arrays.sort(numbers);
+            if(numbers.length % 2 == 0){
+                return (double)(numbers[numbers.length/2 - 1] + numbers[numbers.length / 2]) / 2;
+            }
+            else{
+                return numbers[numbers.length/2];
+            }
+        }
+        System.out.println("there is no median because there are no elements");
+        return 0;
+    }
 }
